8b897b60912d05be9644175e9e22485a9f512a5e
王兴欧 <121489380+XingOuWang@users.noreply.github.com>
2023-01-02 17:19:30 +0800

    打印小飞机

---
+++ exercise03/exercise03/test.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//对于n位的两个正整数x和y，可以把x表示为x1​x2​…xn​，其中xi（1≤i≤n）表示整数x第i位上的数字；把y表示为y1
+//y2…yn，其中yi（1≤i≤n）表示整数y第i位上的数字。如果x1 + k= yn−k（0≤k≤n−1）始终成立，则x和y互为逆序整数。
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	int n;
+	scanf("%d\n",&n);
+
+
+	char a[10];
+	char b[10];
+
+	for (i; i <= n; i++) {
+		scanf("%c",&a[i]);
+	}
+
+	
+
+	
+	
+
+
+	return 0;
+}
